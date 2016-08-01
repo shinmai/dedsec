624fb5eb17cddc3621e0775fcf3b3abfe55a77f6
Kévin Dupas <dupas.kevin@gmail.com>
2016-08-01 23:55:39 +0200

    Dedsec
    Fan Project for Watch Dogs 2

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function change() {
+        document.getElementById("sure").innerHTML ="ARE YOU SURE THAT YOU'RE SURE ?";
+        document.getElementById("wtf").innerHTML ="#WTF";
+        document.getElementById("no").innerHTML ="NO";
+        document.getElementById("background").style.background = "url('img/bg.png')";
+}
